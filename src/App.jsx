92fa21c8e4209f9cb3daefbe8ddef4ef136a166d
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-07-25 16:04:20 -0700

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import './App.css';
 import Home from './Pages/Home/Home';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-    <Home/>
-    </div>
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+       </Routes>
+    </BrowserRouter>
   )
 }
 
