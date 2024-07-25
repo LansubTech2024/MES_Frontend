5e94b2b8cc952299d81dace8646d5b859f0fde3f
KeerthiKrish8 <Keerthikak0393@gmail.com>
2024-07-25 15:58:27 +0530

    add some changes

--- src/Components/Chart/Linechart.jsx
+++ src/Components/Chart/Linechart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import {
     Chart as ChartJS, 
 LineElement, 
@@ -22,13 +22,11 @@ const data={
       data: [10,20,5],
       backgroundColor: [
         "rgba(255.99,132,0.2)",
-        "rgba(54,162,235,0.2)",
-        "rgba(255,206,86,0.2)",
+        
       ],
       borderColor:[
         "rgba(255,99,132,1)",
-        "rgba(54,162,235,1)",
-        "rgba(255,206,86,1)",
+       
       ],
       borderWidth:1
     },
