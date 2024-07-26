dd89791aeb06d588ec457e972fe7df2e08b6e76a
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-07-25 21:51:54 -0700

    changes in sidebar

--- src/Components/SideBar/Sidebar.jsx
+++ src/Components/SideBar/Sidebar.jsx
@@ -21,110 +21,119 @@ const Sidebar = () => {
     }
   return (
       <aside className="sidebar no-scrollbar">
-        <nav>
+        <nav className="sidebar-menu">
           <ul className="list-item">
             <li>
-              <a href="#">
+              <Link href="#">
               <i className="fa-solid fa-gauge-high"></i>Overview
-              </a>
-            </li>
-            <li>
-              <a href="#">
-              <i className="fa-solid fa-gears"></i>Production
-              </a>
-            </li>
-            <li>
-              <a href="#">
-              <i className="fa-solid fa-wrench"></i>Maintenance
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa-solid fa-boxes"></i>Inventory
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa-solid fa-file-lines"></i>Reports
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa-solid fa-clipboard-check"></i>Quality Control
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa-solid fa-truck"></i>Supply Chain
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa-solid fa-bolt"></i>Energy Consumption
-              </a>
+              </Link>
             </li>
             <li>
               <Link href="#">
+              <i className="fa-solid fa-gears"></i>Production
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+              <i className="fa-solid fa-wrench"></i>Maintenance
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <i className="fa-solid fa-boxes"></i>Inventory
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <i className="fa-solid fa-file-lines"></i>Reports
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <i className="fa-solid fa-clipboard-check"></i>Quality Control
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <i className="fa-solid fa-truck"></i>Supply Chain
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <i className="fa-solid fa-bolt"></i>Energy Consumption
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="item">
                 <i className="fa-solid fa-cog"></i>Settings
                 {settingOpen ?(
                   <RxChevronUp
                   onClick={CloseSetting}
+                  className="drop-icon"
+                  size={22}
                   />
                 ):
                 (
                  <RxChevronDown 
-                 onClick={ToggleSetting}/>
+                 onClick={ToggleSetting}
+                 className="drop-icon"
+                 size={22}
+                 />
                 )}
               </Link>
               {settingOpen && (
                 <div className="setting-item">
                     <ul className="setting-list-items">
                         <li>
-                            <Link href='#'>User Management</Link>
+                            <Link href='#' className="set-item">User Management</Link>
                         </li>
                         <li>
-                            <Link href='#'>Notifications</Link>
+                            <Link href='#' className="set-item">Notifications</Link>
                         </li>
                         <li>
-                            <Link href='#'>Preferences</Link>
+                            <Link href='#' className="set-item">Preferences</Link>
                         </li>
                         <li>
-                            <Link href='#'>Security</Link>
+                            <Link href='#' className="set-item">Security</Link>
                         </li>
                     </ul>
                 </div>
               )}
             </li>
             <li>
-              <Link href="#">
+              <Link href="#" className="item">
                 <i className="fa-solid fa-question-circle"></i>
                 Help and Support
                 {helpOpen ?(
                     <RxChevronUp
                     onClick={CloseHelp}
+                    className="drop-icon"
+                    size={40}
                     />
                 )
                 :
                 (
                     <RxChevronDown
                     onClick={ToggleHelp}
+                    className="drop-icon"
+                    size={40}
                     />
                 )}
               </Link>
               {helpOpen && (
                 <div className="help-items">
-                    <ul>
+                    <ul className="help-list-items">
                         <li>
-                            <Link href='#'>User Guide</Link>
+                            <Link href='#' className="help-item">User Guide</Link>
                         </li>
                         <li>
-                            <Link href='#'>FAQ</Link>
+                            <Link href='#' className="help-item">FAQ</Link>
                         </li>
                         <li>
-                            <Link href='#'>Contact Support</Link>
+                            <Link href='#' className="help-item">Contact Support</Link>
                         </li>
                         <li>
-                            <Link href='#'>Feedback</Link>
+                            <Link href='#' className="help-item">Feedback</Link>
                         </li>
                     </ul>
                 </div>
