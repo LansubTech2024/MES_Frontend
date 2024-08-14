e3f4aba8c7f25020311e2fe5459f0e0810829d0d
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-08-14 13:00:31 -0700

    changes in dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from '../../Components/Header/Header';
+import Sidebar from '../../Components/SideBar/Sidebar';
+import './Dashboard.css';
 
 function Graphs() {
   const [graphs, setGraphs] = useState([]);
@@ -15,14 +18,24 @@ function Graphs() {
   }, []);
 
   return (
-    <div>
-      {graphs.map((graph, index) => (
-        <div key={index}>
-          <h2>{graph.type} Chart</h2>
-          <img src={`data:image/png;base64,${graph.data}`} alt={`${graph.type} chart`} />
-        </div>
-      ))}
+    <>
+    <Header/>
+    <Sidebar/>
+      <div className="graphs-container">
+      <div className="graphs-grid">
+        {graphs.map((graph, index) => (
+          <div key={index} className="graph-item">
+            <h2 className="graph-title">{graph.type} Chart</h2>
+            <img 
+              className="graph-image"
+              src={`data:image/png;base64,${graph.data}`} 
+              alt={`${graph.type} chart`} 
+            />
+          </div>
+        ))}
+      </div>
     </div>
+    </>
   );
 }
 
