474cd45b93a30326b762d741d177d84a04b75536
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-08-19 14:25:55 -0700

    changes in graphpopup

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../../Components/Header/Header';
 import Sidebar from '../../Components/SideBar/Sidebar';
+import { FaDownload } from 'react-icons/fa6';
 import { Bar, Line, Pie, Scatter } from 'react-chartjs-2';
 import Plot from 'react-plotly.js';
 import {
@@ -16,7 +17,10 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 import './Dashboard.css';
+import GraphPopup from '../../Components/GraphPopup/GraphPopup';
 
 ChartJS.register(
   CategoryScale,
@@ -30,77 +34,95 @@ ChartJS.register(
   Legend
 );
 
+const graphs = [
+  { type: 'bar', title: 'Average Temperatures', key: 'bar_chart' },
+  { type: 'line', title: 'Temperature Trends', key: 'line_chart' },
+  { type: 'pie', title: 'Temperature Distribution', key: 'pie_chart' },
+  { type: 'scatter', title: 'CHW In vs COW Out', key: 'scatter_chart' },
+  { type: 'histogram', title: 'Temperature Histogram', key: 'histogram_data', plot: true },
+  { type: 'heatmap', title: 'Temperature Heatmap', key: 'heatmap_data', plot: true },
+  { type: 'boxplot', title: 'Temperature Box Plot', key: 'box_plot_data', plot: true }
+];
+
 function Dashboard() {
   const [chartData, setChartData] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [selectedGraphType, setSelectedGraphType] = useState(null);
 
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/charts/')
-      .then(response => {
-        setChartData(response.data);
-      })
+      .then(response => setChartData(response.data))
       .catch(error => {
         console.error('Error fetching graph data:', error);
+        // Consider setting an error state here
       });
   }, []);
 
-  if (!chartData) return <div>Loading...</div>;
+  const handleGraphClick = (graphType) => {
+    setSelectedGraphType(graphType);
+    setIsPopupOpen(true);
+  };
+
+  const handleDownload = () => {
+    const input = document.querySelector('.graphs-grid');
+    html2canvas(input).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, 'PNG', 0, 0);
+      pdf.save('graphs.pdf');
+    });
+  };
+
+  if (!chartData) return <div className="loading">Loading...</div>;
 
   return (
     <>
-    <Header/>
-    <Sidebar/>
-    <div className="graphs-container">
-      <div className="graphs-grid">
-        <div className="graph-item">
-          <h2 className="graph-title">Average Temperatures</h2>
-          <Bar data={chartData.bar_chart} />
-        </div>
-        <div className="graph-item">
-          <h2 className="graph-title">Temperature Trends</h2>
-          <Line data={chartData.line_chart} />
-        </div>
-        <div className="graph-item">
-          <h2 className="graph-title">CHW In Temperature Distribution</h2>
-          <Pie data={chartData.pie_chart} />
-        </div>
-        <div className="graph-item">
-          <h2 className="graph-title">CHW In vs COW Out</h2>
-          <Scatter data={chartData.scatter_chart} />
-        </div>
-        <div className="graph-item">
-          <h2 className="graph-title">Temperature Heatmap</h2>
-          <Plot
-            data={[{
-              z: chartData.heatmap_data.z,
-              x: chartData.heatmap_data.x,
-              y: chartData.heatmap_data.y,
-              type: 'heatmap',
-              colorscale: 'Viridis',
-            }]}
-            layout={{ width: 500, height: 400, title: 'Temperature Heatmap' }}
-          />
-        </div>
-        <div className="graph-item">
-          <h2 className="graph-title">Temperature Histogram</h2>
-          <Plot
-            data={[
-              { x: chartData.histogram_data.chw_in, type: 'histogram', name: 'CHW In' },
-              { x: chartData.histogram_data.chw_out, type: 'histogram', name: 'CHW Out' },
-              { x: chartData.histogram_data.cow_in, type: 'histogram', name: 'COW In' },
-              { x: chartData.histogram_data.cow_out, type: 'histogram', name: 'COW Out' },
-            ]}
-            layout={{ width: 500, height: 400, title: 'Temperature Distribution', barmode: 'overlay' }}
-          />
-        </div>
-        <div className="graph-item">
-          <h2 className="graph-title">Temperature Box Plot</h2>
-          <Plot
-            data={chartData.box_plot_data.map(item => ({ ...item, type: 'box' }))}
-            layout={{ width: 500, height: 400, title: 'Temperature Box Plot' }}
-          />
+      <Header />
+      <Sidebar />
+      <div className="graphs-container">
+      <button className="download-btn" onClick={handleDownload}>
+        <FaDownload size={22} color='blue' className='fa-down'/>
+      </button>
+        <div className="graphs-grid">
+          {graphs.map(({ type, title, key, plot }) => (
+            <div key={type} className="graph-item" onClick={() => handleGraphClick(type)}>
+              <h2 className="graph-title">{title}</h2>
+              {plot ? (
+                <Plot
+                  data={type === 'histogram' ? [
+                    { x: chartData.histogram_data.chw_in, type: 'histogram', name: 'CHW In' },
+                    { x: chartData.histogram_data.chw_out, type: 'histogram', name: 'CHW Out' },
+                    { x: chartData.histogram_data.cow_in, type: 'histogram', name: 'COW In' },
+                    { x: chartData.histogram_data.cow_out, type: 'histogram', name: 'COW Out' }
+                  ] : type === 'heatmap' ? [{
+                    z: chartData.heatmap_data.z,
+                    x: chartData.heatmap_data.x,
+                    y: chartData.heatmap_data.y,
+                    type: 'heatmap',
+                    colorscale: 'Viridis'
+                  }] : [
+                    ...chartData.box_plot_data.map(item => ({ ...item, type: 'box' }))
+                  ]}
+                  layout={{ title: title, barmode: type === 'histogram' ? 'overlay' : undefined }}
+                />
+              ) : (
+                React.createElement(type === 'bar' ? Bar :
+                  type === 'line' ? Line :
+                  type === 'pie' ? Pie :
+                  Scatter, 
+                  { data: chartData[key] })
+              )}
+            </div>
+          ))}
         </div>
       </div>
-    </div>
+      {isPopupOpen && (
+        <GraphPopup
+          isOpen={isPopupOpen}
+          onRequestClose={() => setIsPopupOpen(false)}
+          graphType={selectedGraphType}
+        />
+      )}
     </>
   );
 }
